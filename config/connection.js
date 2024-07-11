db2c89637cdbaf93c32972264ef7bd599700e62a
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-07-10 20:52:56 -0700

    1). Tested starting point features. 2). Added Bootstrap to project.

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
+// const path = require( "path" );
+// require('dotenv').config( { path : path.resolve( __dirname, "../.env" ) } );
 require('dotenv').config();
+
 
 let sequelize;
 
