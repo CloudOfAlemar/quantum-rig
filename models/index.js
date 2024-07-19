ebc997062f56946a7653848b1990391bca03a168
Renz <renzlakers24@gmail.com>
2024-07-18 18:54:53 -0700

    deleted unnecessary files

--- models/index.js
+++ models/index.js
@@ -1,28 +1,6 @@
 const Guest = require('./Guest');
 const PC_build = require('./PC_build');
 const Part = require('./Part');
-const PC_build_parts = require('./PC_build_parts');
-
-// PC_build.belongsTo(Guest, {
-//   foreignKey: 'guest_id'
-// });
-
-// Guest.hasMany(PC_build, {
-//   foreignKey: 'guest_id'
-// });
-
-// PC_build.belongsToMany(Part, { 
-//   through: {
-//     model: PC_build_parts,
-//     unique: false
-//   }
-// });
-
-// Part.belongsToMany(PC_build, { 
-//   through: {
-//     model: PC_build_parts,
-//     unique: false
-//   } });
 
 Guest.hasMany(PC_build, {
   foreignKey: 'guest_id',
@@ -42,4 +20,4 @@ Part.belongsTo(PC_build, {
   foreignKey: 'pc_build_id'
 });
 
-module.exports = { Guest, PC_build, Part, PC_build_parts };
+module.exports = { Guest, PC_build, Part};
